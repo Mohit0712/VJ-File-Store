c1ee76b7c470d8767efb808fd99741dd8ef8a72c
Mohit0712 <143384097+Mohit0712@users.noreply.github.com>
2024-02-14 23:42:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,15 +6,13 @@
 class script(object):
     START_TXT = """<b>Hᴇʟʟᴏ {},ᴍʏ ɴᴀᴍᴇ {},【ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ】ᴀɴᴅ ᴘᴏᴡᴇʀꜰᴜʟ ꜰɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ +└ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ┘+└sᴛʀᴇᴀᴍ / ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ғᴇᴀᴛᴜʀᴇ┘+├ᴄᴜꜱᴛᴏᴍ ᴜʀʟ ꜱʜᴏʀᴛɴᴇʀ ꜱᴜᴘᴘᴏʀᴛ┤+  ᢵᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ sᴜᴘᴘᴏʀᴛ ᢴ ᢾᴀɴᴅ ʙᴇꜱᴛ ᴜɪ ᴘᴇʀꜰᴏʀᴍᴀɴᴄᴇᢿ</b>"""
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : <a href = "https://AdultBazar.com.tr"> @AdultBazarMovies</a>{file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : <a href = "https://AdultBazar.com.tr"> @AdultBazarMovies </a>{file_name}
 
-• Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/AdultBazar)</b>
-• Free Horror Movies :- <a href="https://BhootBazarMovies.com"> Click Here </a>""" 
+• Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/AdultBazar)
+• 𝗙𝗿𝗲𝗲 𝗛𝗼𝗿𝗿𝗼𝗿 𝗠𝗼𝘃𝗶𝗲𝘀 :- :- <a href="https://BhootBazarMovies.com"> 𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲 </a></b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
