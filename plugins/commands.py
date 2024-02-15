759355612eb599fa8c9c32be3ed8c373e7a69919
Mohit0712 <143384097+Mohit0712@users.noreply.github.com>
2024-02-15 14:20:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -151,7 +151,7 @@ async def start(client, message):
         sts = await message.reply("**🔺 ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ**")
         b_string = data.split("-", 1)[1]
         decoded = (base64.urlsafe_b64decode(b_string + "=" * (-len(b_string) % 4))).decode("ascii")
-        await message.reply_text(f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} mins</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
+        await message.reply_text(f"<b>Powred By :- <a href="https://BhootBazarMovies"> Bhoot Bazar Movies </a>")
                
         try:
             f_msg_id, l_msg_id, f_chat_id, protect = decoded.split("_", 3)
@@ -173,7 +173,7 @@ async def start(client, message):
                     file_name = getattr(media, 'file_name', '')
                     f_caption = getattr(msg, 'caption', file_name)
                 try:
-                    h = await message.reply_text(f"<b>Powred By :- <a href="https://BhootBazarMovies"> Bhoot Bazar Movies </a> {AUTO_DELETE} </b>")
+                    h = await message.reply_text(f"<b>Powred By :- <a href="https://BhootBazarMovies"> Bhoot Bazar Movies </a>  </b>")
                     k = await msg.copy(message.chat.id, caption=f_caption, protect_content=True if protect == "/pbatch" else False)
                     
                     asyncio.create_task(delete_after_delay(k, AUTO_DELETE_TIME))
